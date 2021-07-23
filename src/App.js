d21101efd3ea19ca9431af86461a1fa2a7c250be
Ardit Daku <arditdaku1@gmail.com>
2021-07-23 11:56:10 +0200

    Rolobox Monster

--- src/App.js
+++ src/App.js
@@ -1,23 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
+import { CardList } from "./components/card-list/card-list.component";
+import { SearchBox } from "./components/search-box/search-box.component";
 
 function App() {
+  const [users, setUsers] = useState([]);
+  const [search, setSearch] = useState("");
+  useEffect(() =>
+    fetch("https://jsonplaceholder.typicode.com/users")
+      .then((res) => res.json())
+      .then((data) => setUsers(data))
+  );
+  const monstes = users;
+  const searchField = search;
+  const filteredMonsters = monstes.filter((monster) =>
+    monster.name.toLowerCase().includes(searchField.toLowerCase())
+  );
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="mt-3 min-h-screen flex flex-col h-2/5 dark:bg-gray-800 bg-white">
+      <SearchBox
+        placeholder="Search monsters"
+        handleChange={(e) => setSearch(e.target.value)}
+      />
+      <CardList monsters={filteredMonsters} />
     </div>
   );
 }
